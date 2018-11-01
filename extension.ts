e9f67e86dd50eb40fcd609608ec7a5d020550d31
yorkyao <yao.yao@arkie.ai>
2018-11-01 17:31:11 +0800

    feat: only lint the saved file rather than whole project, to improve performance

--- extension.ts
+++ extension.ts
@@ -1,5 +1,6 @@
 import vscode = require("vscode");
 import typeCoverage = require("type-coverage");
+import * as path from 'path';
 const packageJson = require("./package.json");
 
 const extensionDisplayName = packageJson.displayName;
@@ -7,10 +8,16 @@ const extensionDisplayName = packageJson.displayName;
 let outputChannel: vscode.OutputChannel;
 let diagnosticCollection: vscode.DiagnosticCollection;
 
-function lint() {
+function lint(textDocument?: vscode.TextDocument) {
+	const file = textDocument
+		? path.relative(vscode.workspace.rootPath, textDocument.fileName)
+		: undefined
 	typeCoverage.lint(vscode.workspace.rootPath, true, false).then((result) => {
 		const diagnosticsMap = new Map<string, vscode.Diagnostic[]>()
 		for (const any of result.anys) {
+			if (file && file !== path.relative(vscode.workspace.rootPath, any.file)) {
+				continue
+			}
 			const diagnostic = new vscode.Diagnostic(
 				new vscode.Range(
 					new vscode.Position(any.line, any.character),
@@ -25,7 +32,9 @@ function lint() {
 			}
 			diagnosticsMap.get(any.file).push(diagnostic);
 		}
-		diagnosticCollection.clear()
+		if (textDocument) {
+			diagnosticCollection.delete(vscode.Uri.file(textDocument.fileName));
+		}
 		for (const [file, diagnostics] of diagnosticsMap) {
 			diagnosticCollection.set(vscode.Uri.file(file), diagnostics);
 		}
@@ -39,8 +48,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(outputChannel);
 
 	context.subscriptions.push(
-		vscode.workspace.onDidSaveTextDocument(() => {
-			lint();
+		vscode.workspace.onDidSaveTextDocument((textDocument) => {
+			lint(textDocument);
 		}),
 	);
 
