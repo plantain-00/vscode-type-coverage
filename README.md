21fe685e93f8bbd62d372d774785a22b7f989ee0
yorkyao <yao.yao@arkie.ai>
2018-11-01 18:03:53 +0800

    feat: add lint, badge, CI

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 VSCode plugin for type-coverage.
 
+[![Dependency Status](https://david-dm.org/plantain-00/vscode-type-coverage.svg)](https://david-dm.org/plantain-00/vscode-type-coverage)
+[![devDependency Status](https://david-dm.org/plantain-00/vscode-type-coverage/dev-status.svg)](https://david-dm.org/plantain-00/vscode-type-coverage#info=devDependencies)
+[![Build Status: Linux](https://travis-ci.org/plantain-00/vscode-type-coverage.svg?branch=master)](https://travis-ci.org/plantain-00/vscode-type-coverage)
+[![Build Status: Windows](https://ci.appveyor.com/api/projects/status/github/plantain-00/vscode-type-coverage?branch=master&svg=true)](https://ci.appveyor.com/project/plantain-00/vscode-type-coverage/branch/master)
+
 ## features
 
 ![1.png](./resources/1.png)
--- extension.ts
+++ extension.ts
@@ -1,59 +1,59 @@
-import vscode = require("vscode");
-import typeCoverage = require("type-coverage");
-import * as path from 'path';
-const packageJson = require("./package.json");
+import * as vscode from 'vscode'
+import * as typeCoverage from 'type-coverage'
+import * as path from 'path'
+const packageJson = require('./package.json')
 
-const extensionDisplayName = packageJson.displayName;
+const extensionDisplayName = packageJson.displayName
 
-let outputChannel: vscode.OutputChannel;
-let diagnosticCollection: vscode.DiagnosticCollection;
+let outputChannel: vscode.OutputChannel
+let diagnosticCollection: vscode.DiagnosticCollection
 
 function lint(textDocument?: vscode.TextDocument) {
-	const file = textDocument
-		? path.relative(vscode.workspace.rootPath, textDocument.fileName)
-		: undefined
-	typeCoverage.lint(vscode.workspace.rootPath, true, false).then((result) => {
-		const diagnosticsMap = new Map<string, vscode.Diagnostic[]>()
-		for (const any of result.anys) {
-			if (file && file !== path.relative(vscode.workspace.rootPath, any.file)) {
-				continue
-			}
-			const diagnostic = new vscode.Diagnostic(
-				new vscode.Range(
-					new vscode.Position(any.line, any.character),
-					new vscode.Position(any.line, any.character + any.text.length)
-				),
-				`The type is 'any'`,
-				vscode.DiagnosticSeverity.Information);
-			diagnostic.code = any.text;
-			diagnostic.source = extensionDisplayName;
-			if (!diagnosticsMap.has(any.file)) {
-				diagnosticsMap.set(any.file, [])
-			}
-			diagnosticsMap.get(any.file).push(diagnostic);
-		}
-		if (textDocument) {
-			diagnosticCollection.delete(vscode.Uri.file(textDocument.fileName));
-		}
-		for (const [file, diagnostics] of diagnosticsMap) {
-			diagnosticCollection.set(vscode.Uri.file(file), diagnostics);
-		}
-	}, (ex) => {
-		outputChannel.appendLine("[" + (new Date()).toLocaleTimeString() + "] " + "ERROR: Exception while linting:\n" + ex.stack);
-	})
+  const file = textDocument
+    ? path.relative(vscode.workspace.rootPath!, textDocument.fileName)
+    : undefined
+  typeCoverage.lint(vscode.workspace.rootPath!, true, false).then((result) => {
+    const diagnosticsMap = new Map<string, vscode.Diagnostic[]>()
+    for (const anyObject of result.anys) {
+      if (file && file !== path.relative(vscode.workspace.rootPath!, anyObject.file)) {
+        continue
+      }
+      const diagnostic = new vscode.Diagnostic(
+        new vscode.Range(
+          new vscode.Position(anyObject.line, anyObject.character),
+          new vscode.Position(anyObject.line, anyObject.character + anyObject.text.length)
+        ),
+        `The type is 'any'`,
+        vscode.DiagnosticSeverity.Information)
+      diagnostic.code = anyObject.text
+      diagnostic.source = extensionDisplayName
+      if (!diagnosticsMap.has(anyObject.file)) {
+        diagnosticsMap.set(anyObject.file, [])
+      }
+      diagnosticsMap.get(anyObject.file)!.push(diagnostic)
+    }
+    if (textDocument) {
+      diagnosticCollection.delete(vscode.Uri.file(textDocument.fileName))
+    }
+    for (const [file, diagnostics] of diagnosticsMap) {
+      diagnosticCollection.set(vscode.Uri.file(file), diagnostics)
+    }
+  }, (ex) => {
+    outputChannel.appendLine('[' + (new Date()).toLocaleTimeString() + '] ' + 'ERROR: Exception while linting:\n' + ex.stack)
+  })
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	outputChannel = vscode.window.createOutputChannel(extensionDisplayName);
-	context.subscriptions.push(outputChannel);
+  outputChannel = vscode.window.createOutputChannel(extensionDisplayName)
+  context.subscriptions.push(outputChannel)
 
-	context.subscriptions.push(
-		vscode.workspace.onDidSaveTextDocument((textDocument) => {
-			lint(textDocument);
-		}),
-	);
+  context.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument((textDocument) => {
+      lint(textDocument)
+    })
+  )
 
-	diagnosticCollection = vscode.languages.createDiagnosticCollection(extensionDisplayName);
-	context.subscriptions.push(diagnosticCollection);
-	lint();
+  diagnosticCollection = vscode.languages.createDiagnosticCollection(extensionDisplayName)
+  context.subscriptions.push(diagnosticCollection)
+  lint()
 }
