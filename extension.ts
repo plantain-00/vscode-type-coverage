34943a6d53b534899963298543d1efbc69ba010c
yorkyao <yao.yao@arkie.ai>
2018-11-01 13:45:07 +0800

    feat: add basic work code

---
+++ extension.ts
@@ -0,0 +1,49 @@
+import vscode = require("vscode");
+import typeCoverage = require("type-coverage");
+const packageJson = require("./package.json");
+
+const extensionDisplayName = packageJson.displayName;
+
+let outputChannel: vscode.OutputChannel;
+let diagnosticCollection: vscode.DiagnosticCollection;
+
+function lint() {
+	typeCoverage.lint(vscode.workspace.rootPath, true, false).then((result) => {
+		const diagnosticsMap = new Map<string, vscode.Diagnostic[]>()
+		for (const any of result.anys) {
+			const diagnostic = new vscode.Diagnostic(
+				new vscode.Range(
+					new vscode.Position(any.line, any.character),
+					new vscode.Position(any.line, any.character + any.text.length)
+				),
+				any.file,
+				vscode.DiagnosticSeverity.Warning);
+			diagnostic.code = any.text;
+			diagnostic.source = extensionDisplayName;
+			if (!diagnosticsMap.has(any.file)) {
+				diagnosticsMap.set(any.file, [])
+			}
+			diagnosticsMap.get(any.file).push(diagnostic);
+		}
+		for (const [file, diagnostics] of diagnosticsMap) {
+			diagnosticCollection.set(vscode.Uri.file(file), diagnostics);
+		}
+	}, (ex) => {
+		outputChannel.appendLine("[" + (new Date()).toLocaleTimeString() + "] " + "ERROR: Exception while linting:\n" + ex.stack);
+	})
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	outputChannel = vscode.window.createOutputChannel(extensionDisplayName);
+	context.subscriptions.push(outputChannel);
+
+	context.subscriptions.push(
+		vscode.workspace.onDidSaveTextDocument(() => {
+			lint();
+		}),
+	);
+
+	diagnosticCollection = vscode.languages.createDiagnosticCollection(extensionDisplayName);
+	context.subscriptions.push(diagnosticCollection);
+	lint();
+}
