dd7a6294f610381b71c2a09481a68a5b3af47ac2
york yao <yaoao12306@outlook.com>
2018-11-03 18:17:12 +0800

    chore: update dependencies

--- extension.ts
+++ extension.ts
@@ -6,7 +6,6 @@ import * as packageJson from './package.json'
 
 const extensionDisplayName = packageJson.displayName
 
-let outputChannel: vscode.OutputChannel
 let diagnosticCollection: vscode.DiagnosticCollection
 let oldProgram: ts.Program | undefined
 
@@ -38,15 +37,10 @@ function lint(textDocument?: vscode.TextDocument) {
     for (const [file, diagnostics] of diagnosticsMap) {
       diagnosticCollection.set(vscode.Uri.file(file), diagnostics)
     }
-  }, (ex) => {
-    outputChannel.appendLine('[' + (new Date()).toLocaleTimeString() + '] ' + 'ERROR: Exception while linting:\n' + ex.stack)
   })
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  outputChannel = vscode.window.createOutputChannel(extensionDisplayName)
-  context.subscriptions.push(outputChannel)
-
   context.subscriptions.push(
     vscode.workspace.onDidSaveTextDocument((textDocument) => {
       lint(textDocument)
